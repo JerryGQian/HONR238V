80abec7f146a98e11f6a3cec34f9e8bae3843e38
Jerry Qian <jerry@qian.ninja>
2019-03-30 17:39:06 -0400

    more work on vectorization script, gitignore for images, committing emotions and vector files

--- Scripts/VectorizeImages.py
+++ Scripts/VectorizeImages.py
@@ -1,29 +1,33 @@
 import numpy as np
 from PIL import Image #run> pip install Pillow
+import glob
 
 '''
 Converts images and corresponding emotion labels into vectors
 '''
-#n = open("../Data/testFile.txt", "w")
-#n.write("testing yo")
 
-im = Image.open("../Data/Cohn_Kanade_Facial_Emotions/cohn-kanade-images/S005/001/S005_001_00000011.png") # Can be many different formats.
-pix = im.load()
-print(im.size)  # Get the width and hight of the image for iterating over
-print(pix[320,30])  # Get the RGBA Value of the a pixel of an image
-#pix[x,y] = value  # Set the RGBA Value of the image (tuple)
-#im.save('alive_parrot.png')  # Save the modified pixels as .png
+#ipaths = glob.glob("../Data/CK/images/S[0-9]*/[0-9]*/*")
+epaths = glob.glob("../Data/CK/emotions/S[0-9]*/[0-9]*/*")
+#print(paths)
 
-e = open("../Data/Cohn_Kanade_Facial_Emotions/Emotion/S005/001/S005_001_00000011_emotion.txt", "r")
-emotion = e.read().strip()[0]
-print(emotion)
+for epath in epaths:
+	
+	ipath = epath[0:11] + "images" + epath[19:46] + ".png"
+	print(epath)
+	print(ipath)
 
-a = np.empty(shape=(im.size[0] * im.size[1], 1))
-np.savetxt('test1.txt', a, fmt='%d')
+	im = Image.open(ipath) # Can be many different formats.
+	pix = im.load()
 
-v = np.empty(shape=(im.size[0] * im.size[1], 1))
-for x in range(0, im.size[0]):
-	for y in range(0, im.size[1]):
-		v[x*im.size[1] + y] = pix[x,y]
+	e = open(epath, "r")
+	emotion = e.read().strip()[0]
 
-np.savetxt('test2.txt', v, fmt='%d')
+	#copies pixel values into array
+	v = np.empty(shape=(im.size[0] * im.size[1] + 1, 1)) # +1 to fit emotion label at end
+	for x in range(0, im.size[0]):
+		for y in range(0, im.size[1]):
+			v[x*im.size[1] + y] = pix[x,y]
+
+	v[im.size[0]*im.size[1]] = emotion
+
+	np.savetxt("../Data/CK/vectors/" + epath[20:24] + "_" + epath[25:28] + ".txt", v, fmt='%d')
